534a5c9fce9c77767f6dc1f00fc56f5e77220a75
douglas <douglas.versato@gmail.com>
2017-07-20 22:32:08 -0300

    Criação da Aplicação para efetuar Login
    http://www.devmedia.com.br/java-web-criando-uma-tela-de-login-com-jpa-jsf-primefaces-e-mysql/32456

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,74 @@
+package model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+public class Usuario {
+	
+	/**
+	 * Anotação Id informa que o atributo será o identificador da Classe.
+	 * Column define:
+	 * Nome da Coluna
+	 * Pode receber valores nulos? Falso.
+	 * Unique informa que a tabela terá valor unico. 
+	 * Neste caso, não pode existir dois usuarios com o mesmo nome. 
+	 */
+	@Id 
+	@Column (name="id", nullable=false,unique=true) 
+	private int id;
+	
+	
+	@Column (name="nomeusuario", nullable=false, unique=true)
+	private String nomeUsuario;
+	
+	@Column (name="senha", nullable=false, unique=true)
+	private String senha;
+	
+	/**
+	 * Para iformarmos o tipo de data que será armazenado no baco, argumentos:
+	 * Temporal (Temporaltype.DATE) Formato apenas Data
+	 * Temporal (Temporaltype.TIME) Formato Apenas da hora
+	 * Temporal (Temporaltype.TIMESTAMP) Formato de Data e Hora
+	 */
+	@Column (name="ultimoacesso",  unique=true)
+	@Temporal(TemporalType.DATE)
+	private Date ultimoAcesso;
+
+	
+	//Metodos Getters e Setters 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Date getUltimoAcesso() {
+		return ultimoAcesso;
+	}
+
+	public void setUltimoAcesso(Date ultimoAcesso) {
+		this.ultimoAcesso = ultimoAcesso;
+	}
+}
